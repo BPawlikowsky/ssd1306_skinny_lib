4ae3c834981ac8efb7741680cc611d18674c76aa
Bartosz <bartoszpawlikowsky@gmail.com>
2023-04-26 01:27:48 +0200

    charset working now

--- ssd1306_skinny_lib.ino
+++ ssd1306_skinny_lib.ino
@@ -1,19 +1,15 @@
-#include "charSet.h"
 #include "ssd1306-esp32-lib.hpp"
+#include "stringToChar.hpp"
+#include "fonts/charSet.h"
 
 #define OLED_RESET -1        // Reset pin # (or -1 if sharing Arduino reset pin)
-#define CLK_SPEED 3400000
+#define CLK_SPEED 400000
 
 double currentTime, lastTime;
 
 constexpr size_t I2C_BUFFER_SIZE = 1024;
 
-uint8_t PROGMEM *buffer = (uint8_t *)malloc(BUFFER_LENGTH);
-
-byte yOffset = 16;
-byte xOffset = 16;
-byte incDec = 1;
-byte incDec2 = 1;
+uint8_t *buffer = (uint8_t *)malloc(BUFFER_LENGTH);
 
 void setup() {
   int rstPin = -1;
@@ -24,47 +20,13 @@ void setup() {
   Serial.println(res);
   startupSequence();
   memset((uint8_t *)buffer, 0, BUFFER_LENGTH);  // clear buffer
-  sendBuffer(buffer);
 }
 
 void loop() {
-  sendBuffer(buffer);
-  byte offset = xOffset;
   memset((uint8_t *)buffer, 0, BUFFER_LENGTH);  // clear buffer
-  for (byte i = 0; i < 8; i++) {
-    int numOffset = 0;
-    putToBuffer(offset + i, yOffset, numOne[i], buffer);
-
-    numOffset += 8;
-    putToBuffer(offset + i + numOffset, yOffset, numTwo[i], buffer);
-
-    numOffset += 8;
-    putToBuffer(offset + i + numOffset, yOffset, numThree[i], buffer);
-
-    numOffset += 8;
-    putToBuffer(offset + i + numOffset, yOffset, numFour[i], buffer);
-
-    numOffset += 8;
-    putToBuffer(offset + i + numOffset, yOffset, numFive[i], buffer);
-
-    numOffset += 8;
-    putToBuffer(offset + i + numOffset, yOffset, numSix[i], buffer);
-
-    numOffset += 8;
-    putToBuffer(offset + i + numOffset, yOffset, numSeven[i], buffer);
-
-    numOffset += 8;
-    putToBuffer(offset + i + numOffset, yOffset, numEight[i], buffer);
-
-    numOffset += 8;
-    putToBuffer(offset + i + numOffset, yOffset, numNine[i], buffer);
-
-    numOffset += 8;
-    putToBuffer(offset + i + numOffset, yOffset, numZero[i], buffer);
-  }
-  byte yyy = yOffset;
-  if (yyy >= 55 || yyy <= 0) incDec = -incDec;
-  if (offset >= (128 - 80) || offset <= 0) incDec2 = -incDec2;
-  yOffset += incDec;
-  xOffset += incDec2;
+  // char* text = "HELLO WORLD \nabcdefghijklmnoprstuwvxyz\t1234567890!@#$%^&*()_+-,./?;:[]{}=";
+  char text[100];
+  sprintf(text, "%d", millis());
+  stringToCharArray(text, buffer, (char*)font8, 9, 7, (uint16_t*)lut, 10, 0);
+  sendBuffer(buffer);
 }
---
+++ stringToChar.cpp
@@ -0,0 +1,97 @@
+#include "pgmspace.h"
+#include "stringToChar.hpp"
+#include <Arduino.h>
+char* binString(unsigned short n) {
+  char *bin = (char*)malloc(9 * sizeof(char));
+  int x;
+
+  for (x = 0; x < 8; x++) {
+    bin[x] = n & 0b10000000 ? '1' : '0';
+    n <<= 1;
+  }
+  bin[8] = '\0';
+
+  return (bin);
+}
+void getGlyph(char* font, uint8_t* arr) {
+  for (int i = 0; i < 9; i++) {
+    arr[i] = font[i];
+  }
+}
+
+void rotateGlyph90CW(uint8_t* arr, int height) {
+  int size = height * 2;
+  uint8_t* arrTemp = (uint8_t*)malloc(size * sizeof(uint8_t));
+  uint8_t* arrRot = (uint8_t*)malloc(size * sizeof(uint8_t));
+  memset(arrTemp, 0, size);
+  memset(arrRot, 0, size);
+  for (int x = 0; x < height; x++) {
+    for (int y = 0; y < height; y++) {
+      copyPixel(arr, arrTemp, x, y, height - y - 1, x);
+    }
+  }
+  for (int x = 0; x < height; x++) {
+    for (int y = 0; y < height; y++) {
+      copyPixel(arrTemp, arrRot, x, y, height - x - 1, (height - y) - 3);
+    }
+  }
+  for (int i = 0; i < size; i++) {
+    *arr++ = arrRot[i];
+  }
+  free(arrTemp);
+  free(arrRot);
+}
+
+void copyPixel(uint8_t* src, uint8_t* dest, int srcX, int srcY, int destX, int destY) {
+  int srcOffset = srcY - ((srcY / 8) * 8);
+  int destOffset = destY - ((destY / 8) * 8);
+  uint8_t mask = (0b00000001 << (7 - srcOffset));
+  uint8_t srcByte = src[srcX + (128 * (srcY / 8))] & mask;
+  srcByte = srcByte >> (7 - srcOffset);
+  uint8_t destByte = dest[destX + (128 * (destY / 8))] | (((srcByte == 1) ? 0b00000001 : 0x00) << (7 - destOffset));
+  dest[destX + (128 * (destY / 8))] = destByte;
+}
+
+void stringToCharArray(char* text, uint8_t* buffer, char* font, int fontHeight, int fontWidth, uint16_t* lut, int x, int y) {
+  // Map text to glyphs
+  char* p = text;
+  char character;
+  uint8_t* bp = buffer;
+  uint8_t* arr = (uint8_t*)malloc(fontHeight * 2 * sizeof(uint8_t));
+  memset(arr, 0, fontHeight * 2 * sizeof(uint8_t));
+
+  while ((character = *p) != '\0') {
+    int pageOffset = bp - buffer;
+    int endOfPageDist = 128 - pageOffset%128;
+
+    if(bp - buffer > 128 * 8) {
+      break;
+    }
+
+    if((128 - pageOffset%128) <= fontWidth) {
+        bp+=(fontWidth + x);
+    }
+    Serial.printf("End of Page dist: %d\n", endOfPageDist);
+
+    if(character == '\n') {
+      bp+=(endOfPageDist + x - fontWidth);
+    } else if(character == '\t') {
+      bp+=(fontWidth * 3);
+    } else {
+      int offset = ((uint8_t)character) - ((uint8_t)' ');
+      char* fp = &font[lut[offset]];
+      
+      getGlyph(fp, arr);
+      rotateGlyph90CW(arr, fontHeight);
+      for (int i = 0; i < fontWidth; i++) {
+        bp[i + x + (128 * (y / 8))] = arr[i];
+        bp[i + 128 + x + (128 * (y / 8))] = arr[i + 9];
+      }
+
+      bp += fontWidth;
+    }
+    p++;
+  }
+
+  free(arr);
+}
