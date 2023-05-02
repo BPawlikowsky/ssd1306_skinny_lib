9847da80ccd5c710559420f76d255988b6b35b10
Bartosz <bartoszpawlikowsky@gmail.com>
2023-05-02 17:45:27 +0200

    running tests and such

--- ssd1306_skinny_lib.ino
+++ ssd1306_skinny_lib.ino
@@ -11,10 +11,10 @@ constexpr size_t I2C_BUFFER_SIZE = 1024;
 uint8_t *buffer = (uint8_t *)malloc(BUFFER_LENGTH);
 
 char *testText = "HELLO WORLD\nabcdefghijklmnoprstuwvxyz\tABCDEFGHIJKLMNOPRSTUWVXYZ\t1234567890!@#$%^&*()_+-,./?;:[]{}=<>\"\'`~|";
-char text[100] = "0";
+char fpsString[100] = "0";
 float x = 0.0f; float y = 0.0f;
 float xIncDec = 0.5f;
-float yIncDec = 1.0f;
+float yIncDec = 0.5f;
 int fps = 0;
 int frames = 0;
 
@@ -35,17 +35,17 @@ void loop() {
   start = micros();
   memset((uint8_t *)buffer, 0, BUFFER_LENGTH);  // clear buffer
   // Basic text block tests
-  printStringToBuffer(testText, buffer, x, 0, 1);
-  // printStringToBuffer(testText, buffer, x, 0, 2);
-  // printStringToBuffer(testText, buffer, x, 0, 3);
-  // printStringToBuffer(testText, buffer, x, 0, 4);
-  // printStringToBuffer(testText, buffer, x, 0, 5);
-  // printStringToBuffer(testText, buffer, x, 0, 6);
-  // printStringToBuffer(testText, buffer, x, 0, 7);
-  // printStringToBuffer(testText, buffer, x, 0, 8);
+  // printStringToBuffer(testText, buffer, 128, x, 0, 1);
+  // printStringToBuffer(testText, buffer, 128, x, 0, 2);
+  // printStringToBuffer(testText, buffer, 128, x, 0, 3);
+  // printStringToBuffer(testText, buffer, 128, x, 0, 4);
+  // printStringToBuffer(testText, buffer, 128, x, 0, 5);
+  // printStringToBuffer(testText, buffer, 128, x, 0, 6);
+  // printStringToBuffer(testText, buffer, 128, x, 0, 7);
+  // printStringToBuffer(testText, buffer, 128, x, 0, 8);
 
   // FPS counter
-  printStringToBuffer(text, buffer, 0, 0, 1);
+  printStringToBuffer(fpsString, buffer, 128, 5, 0, 8);
   sendBuffer(buffer); // send buffer to ssd1306
   x+=xIncDec;
   y+=yIncDec;
@@ -56,5 +56,5 @@ void loop() {
   // Serial.printf("%s\n", binString(font[1]));
   // font+=2;
   
-  sprintf(text, "%0.2ffps", (1000000 / (end - start)));
+  sprintf(fpsString, "%0.2ffps", (1000000 / (end - start)));
 }
